2d0b40938242f45a0f25320f30c12cac36f2db31
GonoyaPedrus <bourdelp@gmail.com>
2024-03-12 09:17:27 +0100

    push create_team

--- src/create_team.py
+++ src/create_team.py
@@ -9,7 +9,7 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates") 
 new_saison = "2023-24"
-dict_team = [{'name': 'Erling Haaland', 'position': 'FWD', 'team': 'Man City', 'cost': 115, 'predicted_performance': 16.540115}, {'name': 'Bruno Miguel Borges Fernandes', 'position': 'MID', 'team': 'Man Utd', 'cost': 105, 'predicted_performance': 11.457617}, {'name': 'David De Gea Quintana', 'position': 'GK', 'team': 'Man Utd', 'cost': 50, 'predicted_performance': 10.829479}, {'name': 'Alisson Ramses Becker', 'position': 'GK', 'team': 'Liverpool', 'cost': 60, 'predicted_performance': 10.573772}, {'name': 'Bruno Borges Fernandes', 'position': 'MID', 'team': 'Man Utd', 'cost': 100, 'predicted_performance': 10.408141}, {'name': 'Ivan Toney', 'position': 'FWD', 'team': 'Brentford', 'cost': 65, 'predicted_performance': 10.241132}, {'name': 'Cody Gakpo', 'position': 'MID', 'team': 'Liverpool', 'cost': 80, 'predicted_performance': 9.668974}, {'name': 'James Maddison', 'position': 'MID', 'team': 'Leicester', 'cost': 70, 'predicted_performance': 9.51857}, {'name': 'Kaoru Mitoma', 'position': 'MID', 'team': 'Brighton', 'cost': 50, 'predicted_performance': 9.316569}, {'name': 'Romelu Lukaku', 'position': 'FWD', 'team': "nan", 'cost': 90, 'predicted_performance': 9.279969}, {'name': 'Andrew Robertson', 'position': 'DEF', 'team': "nan", 'cost': 45, 'predicted_performance': 8.692561}, {'name': 'Trent Alexander-Arnold', 'position': 'DEF', 'team': "nan", 'cost': 45, 'predicted_performance': 8.22334}, {'name': 'Antonio Rüdiger', 'position': 'DEF', 'team': "nan", 'cost': 60, 'predicted_performance': 8.159134}, {'name': 'Rico Henry', 'position': 'DEF', 'team': 'Brentford', 'cost': 45, 'predicted_performance': 7.671472}]
+dict_team = [{'name': 'Erling Haaland', 'element_type': 'FWD', 'team': 'Man City', 'cost': 115, 'predicted_performance': 16.540115}, {'name': 'Bruno Miguel Borges Fernandes', 'element_type': 'MID', 'team': 'Man Utd', 'cost': 105, 'predicted_performance': 11.457617}, {'name': 'David De Gea Quintana', 'element_type': 'GK', 'team': 'Man Utd', 'cost': 50, 'predicted_performance': 10.829479}, {'name': 'Alisson Ramses Becker', 'element_type': 'GK', 'team': 'Liverpool', 'cost': 60, 'predicted_performance': 10.573772}, {'name': 'Bruno Borges Fernandes', 'element_type': 'MID', 'team': 'Man Utd', 'cost': 100, 'predicted_performance': 10.408141}, {'name': 'Ivan Toney', 'element_type': 'FWD', 'team': 'Brentford', 'cost': 65, 'predicted_performance': 10.241132}, {'name': 'Cody Gakpo', 'element_type': 'MID', 'team': 'Liverpool', 'cost': 80, 'predicted_performance': 9.668974}, {'name': 'James Maddison', 'element_type': 'MID', 'team': 'Leicester', 'cost': 70, 'predicted_performance': 9.51857}, {'name': 'Kaoru Mitoma', 'element_type': 'MID', 'team': 'Brighton', 'cost': 50, 'predicted_performance': 9.316569}, {'name': 'Romelu Lukaku', 'element_type': 'FWD', 'team': "nan", 'cost': 90, 'predicted_performance': 9.279969}, {'name': 'Andrew Robertson', 'element_type': 'DEF', 'team': "nan", 'cost': 45, 'predicted_performance': 8.692561}, {'name': 'Trent Alexander-Arnold', 'element_type': 'DEF', 'team': "nan", 'cost': 45, 'predicted_performance': 8.22334}, {'name': 'Antonio Rüdiger', 'element_type': 'DEF', 'team': "nan", 'cost': 60, 'predicted_performance': 8.159134}, {'name': 'Rico Henry', 'element_type': 'DEF', 'team': 'Brentford', 'cost': 45, 'predicted_performance': 7.671472}]
 def new_saison_player_index(new_saison):
     list_player = os.listdir(f"../data/{new_saison}/players")
     #map list_player for split by _ and get first and second name
@@ -23,6 +23,7 @@ dict_index = new_saison_player_index(new_saison)
 def new_saison_player(new_saison,dict_index):
     df = pd.read_csv(f"../data/{new_saison}/cleaned_players.csv")
     df_2 = pd.read_csv(f"../data/{new_saison}/players_raw.csv")
+    df_teams = pd.read_csv(f"../data/{new_saison}/teams.csv")
     #get a liste of player with column first_name and second_name
     list_player = df["first_name"].str.cat(df["second_name"], sep=" ").tolist()
     liste_element = df["element_type"].tolist()
@@ -35,9 +36,13 @@ def new_saison_player(new_saison,dict_index):
             dict_tmp["id"] = v
             dict_tmp["element_type"] = liste_element[list_player.index(k)]
             df_tmp_2 = df_2[(df_2["id"] == int(v))]
-            dict_tmp["team"] = df_tmp_2["team"].values[0]
+            dict_tmp["cost"] = df_tmp_2["now_cost"].values[0]
+            df_tmp_3 = df_teams[(df_teams["id"] == int(df_tmp_2["team"].values[0]))]
+            dict_tmp["team"] = df_tmp_3["name"].values[0]
+            dict_tmp["predicted_performance"] = 0
             # add v in dict_player["id"]
             dict_player.append(dict_tmp)
+    print(dict_player[:3])
     return dict_player
 
 # Route principale pour afficher la page HTML
--- src/static/script_create.js
+++ src/static/script_create.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const players = document.querySelectorAll('.player');
-    const teamCards = document.querySelectorAll('.team .card');
+    const teamSlots = document.querySelectorAll('.team-slot');
 
     let draggedPlayer = null;
 
@@ -11,25 +11,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Ajoutez des écouteurs d'événements pour les cartes de l'équipe
-    teamCards.forEach(card => {
-        card.addEventListener('dragover', function (e) {
+    // Ajoutez des écouteurs d'événements pour les emplacements d'équipe
+    teamSlots.forEach(slot => {
+        slot.addEventListener('dragover', function (e) {
             e.preventDefault();
         });
 
-        card.addEventListener('drop', function (e) {
+        slot.addEventListener('drop', function (e) {
             e.preventDefault();
             if (draggedPlayer) {
-                // Échangez les joueurs entre la barre latérale et l'équipe
-                const playerName = draggedPlayer.querySelector('span').textContent;
-                const playerPosition = draggedPlayer.querySelector('.element-type').textContent;
-                const cardPlayerName = card.querySelector('p').textContent;
-
-                // Mettez en œuvre la logique pour échanger les joueurs
-                // Vous devrez ajuster votre logique pour gérer correctement les échanges de joueurs dans votre application
-                draggedPlayer.querySelector('span').textContent = cardPlayerName;
-                card.querySelector('p').textContent = playerName;
-
+                // Retirer le joueur existant dans la fente d'équipe (s'il y en a un)
+                const existingPlayer = this.querySelector('.player');
+                if (existingPlayer) {
+                    // Réinitialiser la position du joueur existant à la barre latérale
+                    document.getElementById('players-container').appendChild(existingPlayer);
+                }
+                // Ajouter le joueur glissé à l'emplacement de l'équipe
+                this.appendChild(draggedPlayer);
+                // Réinitialiser la variable
                 draggedPlayer = null;
             }
         });
