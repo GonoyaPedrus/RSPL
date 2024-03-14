920d8907ed25449aeaf10c82e0a023819e9dfa0f
GonoyaPedrus <bourdelp@gmail.com>
2024-03-14 13:11:14 +0100

    drag and drop only position + cost restant

--- src/static/script_create.js
+++ src/static/script_create.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const teamSlots = document.querySelectorAll('.team-card');
     const remainingAmountSpan = document.getElementById('remaining-amount');
     const totalBudget = 1000; // Budget total disponible
-    const totalCostSpan = document.getElementById('total-cost');
-
     let draggedPlayer = null;
+    let totalCost = 0;
+    const teamIdSpan = document.getElementById('team-id');
+    const teamId = generate_new_team_id();
+    teamIdSpan.textContent = teamId;
 
     // Ajoutez des écouteurs d'événements pour les éléments glissables dans la barre latérale
     players.forEach(player => {
@@ -18,82 +20,131 @@ document.addEventListener('DOMContentLoaded', function () {
         player.addEventListener('dragend', function (e) {
             e.target.classList.remove('dragging'); // Retirer la classe 'dragging' après le glisser-déposer
             updateTeamTotalCost(); // Mettre à jour l'argent restant après chaque glisser-déposer
-
         });
     });
 
     // Ajoutez des écouteurs d'événements pour les emplacements d'équipe
-teamSlots.forEach(slot => {
-    slot.addEventListener('dragover', function (e) {
-        e.preventDefault();
+    teamSlots.forEach(slot => {
+        slot.addEventListener('dragover', function (e) {
+            e.preventDefault();
+        });
+
+        slot.addEventListener('drop', function (e) {
+            e.preventDefault();
+
+            // Récupérer la position de la carte cible (slot)
+            const droppedPosition = slot.getAttribute('card-position');
+            // Récupérer la position de la carte en cours de glissement (draggedPlayer)
+            const draggedPosition = draggedPlayer.getAttribute('card-position');
+
+            // Vérifier si les positions sont différentes
+            if (droppedPosition === draggedPosition) {
+                // Si les positions sont identiques, effectuer le glisser-déposer
+
+                // Échanger les textes et le titre des cartes
+                const tempPlayerTitle = Array.from(this.querySelectorAll('.card-title'));
+                const tempPlayerTexts = Array.from(this.querySelectorAll('.card-text'));
+                const draggedPlayerTitle = Array.from(draggedPlayer.querySelectorAll('.card-title'));
+                const draggedPlayerTexts = Array.from(draggedPlayer.querySelectorAll('.card-text'));
+
+                // Échanger les titres
+                tempPlayerTitle.forEach((element, index) => {
+                    const tempTitle = element.textContent;
+                    element.textContent = draggedPlayerTitle[index].textContent;
+                    draggedPlayerTitle[index].textContent = tempTitle;
+                });
+
+                // Échanger les textes
+                tempPlayerTexts.forEach((element, index) => {
+                    const tempText = element.textContent;
+                    element.textContent = draggedPlayerTexts[index].textContent;
+                    draggedPlayerTexts[index].textContent = tempText;
+                });
+
+            } else {
+                // Si les positions sont différentes, annuler le glisser-déposer
+                return;
+            }
+
+            // Réinitialiser la variable draggedPlayer
+            draggedPlayer = null;
+
+            // Mettre à jour le coût total de l'équipe après chaque glisser-déposer
+            updateTeamTotalCost();
+        });
     });
 
-    slot.addEventListener('drop', function (e) {
-        e.preventDefault();
-
-        // Récupérer la position de la carte cible (slot)
-        const droppedPosition = slot.getAttribute('card-position');
-
-        // Récupérer la position de la carte en cours de glissement (draggedPlayer)
-        const draggedPosition = draggedPlayer.getAttribute('card-position');
-
-        // Vérifier si les positions sont différentes
-        if (droppedPosition === draggedPosition) {
-            // Si les positions sont identiques, effectuer le glisser-déposer
-
-            // Échanger les textes et le titre des cartes
-            const tempPlayerTitle = Array.from(this.querySelectorAll('.card-title'));
-            const tempPlayerTexts = Array.from(this.querySelectorAll('.card-text'));
-            const draggedPlayerTitle = Array.from(draggedPlayer.querySelectorAll('.card-title'));
-            const draggedPlayerTexts = Array.from(draggedPlayer.querySelectorAll('.card-text'));
-
-            // Échanger les titres
-            tempPlayerTitle.forEach((element, index) => {
-                const tempTitle = element.textContent;
-                element.textContent = draggedPlayerTitle[index].textContent;
-                draggedPlayerTitle[index].textContent = tempTitle;
-            });
-
-            // Échanger les textes
-            tempPlayerTexts.forEach((element, index) => {
-                const tempText = element.textContent;
-                element.textContent = draggedPlayerTexts[index].textContent;
-                draggedPlayerTexts[index].textContent = tempText;
-            });
-
+    // Fonction pour extraire la valeur numérique du texte de coût
+    function extractCostValue(costText) {
+        // Utiliser une expression régulière pour rechercher un nombre dans la chaîne
+        const regex = /(\d+)/;
+        const match = costText.match(regex);
+        if (match) {
+            return parseInt(match[0]); // Retourner la valeur numérique trouvée
         } else {
-            // Si les positions sont différentes, annuler le glisser-déposer
-            return;
+            return 0; // Retourner 0 si aucun nombre n'est trouvé
         }
-
-        // Réinitialiser la variable draggedPlayer
-        draggedPlayer = null;
-
-        // Mettre à jour le coût total de l'équipe après chaque glisser-déposer
-        updateTeamTotalCost();
-    });
-});
+    }
 
     // Fonction pour calculer et mettre à jour le coût total de l'équipe
     function updateTeamTotalCost() {
-        let totalCost = 0;
+        totalCost = 0; // Réinitialiser le coût total à 0
         teamSlots.forEach(slot => {
             const costText = slot.querySelector('.card-cost').textContent;
-            const costValue = parseInt(costText);
-            if (!isNaN(costValue)) {
-                totalCost += costValue; // Ajouter le coût de chaque joueur
-            }
+            const costValue = extractCostValue(costText); // Utiliser la fonction pour extraire la valeur numérique
+            totalCost += costValue; // Ajouter le coût de chaque joueur
         });
-        totalCostSpan.textContent = totalCost; // Afficher le coût total de l'équipe
         updateRemainingMoney(); // Mettre à jour l'argent restant
     }
 
     // Fonction pour calculer et mettre à jour l'argent restant
     function updateRemainingMoney() {
-        const remainingMoney = totalBudget - parseInt(totalCostSpan.textContent);
+        const remainingMoney = totalBudget - totalCost; // Calculer l'argent restant
         remainingAmountSpan.textContent = remainingMoney; // Afficher l'argent restant
     }
 
-    // Appel initial pour mettre à jour le coût total de l'équipe
-    updateTeamTotalCost();
+    // Ajouter un gestionnaire d'événements pour le clic sur le bouton "Save"
+    const saveButton = document.getElementById('save-button');
+    saveButton.addEventListener('click', function () {
+        saveTeam(); // Appel de la fonction saveTeam sans argument
+    });
+
+    // Fonction pour générer un nouvel identifiant d'équipe
+    function generate_new_team_id() {
+        return Math.random().toString(36).substr(2, 9); // Génère une chaîne aléatoire de 9 caractères
+    }
+
+    // Définir la fonction pour sauvegarder l'équipe
+    function saveTeam() {
+        // Récupérer les ID des joueurs de l'équipe
+        const playerCards = document.querySelectorAll('.team-card');
+        const playersIds = {};
+        playerCards.forEach(card => {
+            const playerId = card.querySelector('.card-id').textContent; // Correction ici
+            const teamId = document.querySelector('#team-id').textContent;
+            playersIds[playerId] = teamId.toString(); // Convertir en chaîne
+        });
+
+        // Effectuer une requête POST pour sauvegarder l'équipe
+        fetch('/api/save_team', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ players_ids: playersIds })
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                // Gérer la réponse de la sauvegarde (si nécessaire)
+                console.log('Team saved successfully:', data);
+            })
+            .catch(error => {
+                console.error('There was a problem with your fetch operation:', error);
+            });
+    }
 });
